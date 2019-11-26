980766b7a439d4fbca065cd47e7c5216ac78d107
kkozlovsky <kkozlovsky@yandex.ru>
2019-11-27 00:11:43 +0300

    Добавил DataSource

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.postgresql:postgresql")
+	implementation("javax.xml.bind:jaxb-api")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
