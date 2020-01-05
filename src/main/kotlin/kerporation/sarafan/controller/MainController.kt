a3ca47a64811520e18e78d5a6dea149d0d2f04bc
kkozlovsky <kkozlovsky@yandex.ru>
2020-01-05 22:52:03 +0300

    Webpack + фронт на модули

--- src/main/kotlin/kerporation/sarafan/controller/MainController.kt
+++ src/main/kotlin/kerporation/sarafan/controller/MainController.kt
@@ -3,6 +3,7 @@ package kerporation.sarafan.controller
 import kerporation.sarafan.domain.User
 import kerporation.sarafan.repo.MessageRepo
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/")
-class MainController(@Autowired private val messageRepo: MessageRepo) {
+class MainController(@Autowired private val messageRepo: MessageRepo,
+                     @Value("\${spring.profiles.active}") private val profile: String) {
 
     @GetMapping
     fun index(model: Model, @AuthenticationPrincipal user: User?): String {
@@ -19,6 +21,7 @@ class MainController(@Autowired private val messageRepo: MessageRepo) {
         data["profile"] = user
         data["messages"] = messageRepo.findAll()
         model.addAttribute("frontendData", data)
+        model.addAttribute("isDevMode", "dev" == profile)
         return "index"
     }
 }
