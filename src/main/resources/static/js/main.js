8abceeb3bcd8b892750e65a3b1bbc2e2b987a53d
kkozlovsky <kkozlovsky@yandex.ru>
2020-01-06 16:04:34 +0300

    Добавил Vuetify 1.5 stable

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import Vuetify from 'vuetify'
 import App from 'pages/App.vue'
-import { connect } from "./util/ws"
+import { connect } from './util/ws'
+import 'vuetify/dist/vuetify.min.css'
 
 if (frontendData.profile) {
     connect()
 }
 
+Vue.use(Vuetify);
 Vue.use(VueResource);
 
 new Vue({
